213c530a9d7a79659099b995c6a233073ce3f007
blackcat52 <107428992+blackcat52@users.noreply.github.com>
2022-06-29 01:39:29 +1000

    Update test.py

--- test.py
+++ test.py
@@ -89,7 +89,7 @@ colors = ["Blue", "Red", "Yellow", "Orange", "Purple", "Green"]
 color = random.choice(colors)
 
 if st.button('Submit'):
-    st.success(f'Your color is {color}. Click below for your full report.')
+    st.success(f'The color of your is {color}. Click below for your full detailed report.')
     here = st.markdown('<a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/">Click Here</a>', unsafe_allow_html=True)
 
 st_lottie(
