6f1e9ad1a30355f3476ad4ed5c8bb5f39459bc5a
blackcat52 <107428992+blackcat52@users.noreply.github.com>
2022-06-28 19:59:54 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import platform
 import webbrowser
 import random
 
@@ -65,8 +66,9 @@ color = random.choice(colors)
 
 if st.button('Submit'):
     st.success(f'Your color is {color}. You are being redirected to your full description.')
-    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    webbrowser.open_new(url)
+        if platform.system() == 'Windows':
+            url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            webbrowser.open(url)
     
     
     
